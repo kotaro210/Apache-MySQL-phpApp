c183b360ef25bb35121f2e81c9aa46e937a9dccf
Ubuntu <ubuntu@ip-172-31-92-158.ec2.internal>
2022-11-06 13:13:55 +0000

    環境変数の設定をした

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php
 
+require_once __DIR__ . "/vendor/autoload.php";
+
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 // データベースに接続する
 $dsn = 'mysql:dbname=webapp;host=localhost';
 $user = 'webapp';
-$password = 'webappのパスワード';
+$password = $_ENV['DB_PASS'];
+
 
 try{
     $dbh = new PDO($dsn, $user, $password);
